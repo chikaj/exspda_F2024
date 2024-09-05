0d50dcdc0477a856992ae2afb7d8c29a64f51beb
chikaj <ncurrit@gmail.com>
2024-09-04 21:51:31 -0500

    fixed inclass1

---
+++ inclass1/inclass1.sql
@@ -0,0 +1,6 @@
+CREATE TABLE inclass1 (
+    phrase TEXT
+);
+
+INSERT INTO inclass1 (phrase) VALUES
+('     tHIS tEXT IS ALL fUnKy    ');
--- inclass1/tc1.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE tc1 (
-    phrase TEXT
-);
-
-INSERT INTO tc1 (phrase) VALUES
-('     tHIS tEXT IS ALL fUnKy    ');
